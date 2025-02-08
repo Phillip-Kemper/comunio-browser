47106db74622711b414093954eee42104bd0dee1
Phillip-Kemper <33121795+Phillip-Kemper@users.noreply.github.com>
2025-02-08 21:11:25 +0100

    basic openai call

---
+++ main.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about recursion in programming."
+        }
+    ]
+)
+
+print(completion.choices[0].message.content)
+
